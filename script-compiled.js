8e42425befc491cc0ad69e6214e54963a0030ca2
Adrian Meller <melleradria@gmail.com>
2018-07-25 14:17:18 +0200

    Add working files

---
+++ script-compiled.js
@@ -0,0 +1,144 @@
+'use strict';
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var Stopwatch = function () {
+    function Stopwatch(display, results) {
+        _classCallCheck(this, Stopwatch);
+
+        this.running = false;
+        this.display = display;
+        this.reset();
+        this.print(this.times);
+        this.copies = [];
+        this.results = results;
+    }
+
+    _createClass(Stopwatch, [{
+        key: 'start',
+        value: function start() {
+            var _this = this;
+
+            if (!this.running) {
+                this.running = true;
+                this.watch = setInterval(function () {
+                    return _this.step();
+                }, 10);
+            }
+        }
+    }, {
+        key: 'reset',
+        value: function reset() {
+            this.times = {
+                minutes: 0,
+                seconds: 0,
+                miliseconds: 0
+            };
+        }
+    }, {
+        key: 'zero',
+        value: function zero() {
+            this.reset();
+            this.print();
+        }
+    }, {
+        key: 'step',
+        value: function step() {
+            if (!this.running) return;
+            this.calculate();
+            this.print();
+        }
+    }, {
+        key: 'print',
+        value: function print() {
+            this.display.innerText = this.format(this.times);
+        }
+    }, {
+        key: 'format',
+        value: function format(times) {
+            return pad0(times.minutes) + ':' + pad0(times.seconds) + ':' + pad0(Math.floor(times.miliseconds));
+        }
+    }, {
+        key: 'calculate',
+        value: function calculate() {
+            this.times.miliseconds += 1;
+            if (this.times.miliseconds >= 100) {
+                this.times.seconds += 1;
+                this.times.miliseconds = 0;
+            }
+            if (this.times.seconds >= 60) {
+                this.times.minutes += 1;
+                this.times.seconds = 0;
+            }
+        }
+    }, {
+        key: 'stop',
+        value: function stop() {
+            this.running = false;
+            clearInterval(this.watch);
+        }
+    }, {
+        key: 'copy',
+        value: function copy() {
+            this.copies.push(this.format(this.times));
+            this.printCopies();
+        }
+    }, {
+        key: 'printCopies',
+        value: function printCopies() {
+            this.results.innerText = '';
+            var list = document.createDocumentFragment();
+            this.copies.forEach(function (copy) {
+                var element = document.createElement('li');
+                element.innerHTML = copy;
+                list.appendChild(element);
+            });
+            this.results.appendChild(list);
+        }
+    }, {
+        key: 'clear',
+        value: function clear() {
+            this.copies = [];
+            this.printCopies();
+        }
+    }]);
+
+    return Stopwatch;
+}();
+
+function pad0(value) {
+    var result = value.toString();
+    if (result.length < 2) {
+        result = '0' + result;
+    }
+    return result;
+}
+
+var stopwatch = new Stopwatch(document.querySelector('.stopwatch'), document.querySelector('.results'));
+
+var startButton = document.getElementById('start');
+startButton.addEventListener('click', function () {
+    return stopwatch.start();
+});
+
+var stopButton = document.getElementById('stop');
+stopButton.addEventListener('click', function () {
+    return stopwatch.stop();
+});
+
+var resetButton = document.getElementById('reset');
+resetButton.addEventListener('click', function () {
+    return stopwatch.zero();
+});
+
+var copyButton = document.getElementById('copy');
+copyButton.addEventListener('click', function () {
+    return stopwatch.copy();
+});
+
+var clearButton = document.getElementById('clear');
+clearButton.addEventListener('click', function () {
+    return stopwatch.clear();
+});
---
+++ script.js
@@ -0,0 +1,102 @@
+class Stopwatch {
+    constructor(display, results) {
+        this.running = false;
+        this.display = display;
+        this.reset();
+        this.print(this.times);
+        this.copies = [];
+        this.results = results;
+    }
+    start() {
+    	if (!this.running) {
+        	this.running = true;
+        	this.watch = setInterval(() => this.step(), 10);
+    	}
+	}
+
+    reset() {
+        this.times = {
+            minutes: 0,
+            seconds: 0,
+            miliseconds: 0
+        };
+	}
+	zero() {
+		this.reset();
+		this.print();
+	}
+	step() {
+    	if (!this.running) return;
+    	this.calculate();
+    	this.print();
+	}
+	print() {
+        this.display.innerText = this.format(this.times);
+	}
+	format(times) {
+        return `${pad0(times.minutes)}:${pad0(times.seconds)}:${pad0(Math.floor(times.miliseconds))}`;
+	}
+	calculate() {
+    	this.times.miliseconds += 1;
+    	if (this.times.miliseconds >= 100) {
+        	this.times.seconds += 1;
+        	this.times.miliseconds = 0;
+    	}
+    	if (this.times.seconds >= 60) {
+        	this.times.minutes += 1;
+        	this.times.seconds = 0;
+    	}
+	}
+	stop() {
+    	this.running = false;
+    	clearInterval(this.watch);
+	}
+	copy() {
+		this.copies.push(this.format(this.times));
+		this.printCopies();
+	}
+	printCopies() {
+		this.results.innerText = '';
+        let list = document.createDocumentFragment();
+        this.copies.forEach((copy) => {
+            let element = document.createElement('li');
+            element.innerHTML = copy;
+            list.appendChild(element);
+        });
+        this.results.appendChild(list);
+	}
+	  clear() {
+        this.copies = [];
+        this.printCopies();
+    }
+
+}
+
+function pad0(value) {
+    let result = value.toString();
+    if (result.length < 2) {
+        result = '0' + result;
+    }
+    return result;
+}
+
+
+const stopwatch = new Stopwatch(document.querySelector('.stopwatch'), document.querySelector('.results'));
+
+let startButton = document.getElementById('start');
+startButton.addEventListener('click', () => stopwatch.start());
+
+let stopButton = document.getElementById('stop');
+stopButton.addEventListener('click', () => stopwatch.stop());
+
+let resetButton = document.getElementById('reset');
+resetButton.addEventListener('click', () => stopwatch.zero());
+
+let copyButton = document.getElementById('copy');
+copyButton.addEventListener('click', () => stopwatch.copy());
+
+let clearButton = document.getElementById('clear');
+clearButton.addEventListener('click', () => stopwatch.clear());
+
+
+
